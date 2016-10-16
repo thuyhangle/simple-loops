b0c996e77b09a1b74e040e4194c7712bc95d4114
Le Thuy Hang <t4leth00@students.oamk.fi>
2016-10-16 03:07:46 +0300

    Morse converter in taskC8

---
+++ taskC8/default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="taskC8._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>ASP.NET</h1>
+        <p>C8 Create a morse code converter.</p>
+        <br />
+        <div class="form-group">
+            <asp:Label runat ="server" Text="Input"></asp:Label>
+            <br />
+            <asp:TextBox ID="input" CssClass="form-control" runat="server" Width="800" Height="100"></asp:TextBox>
+        </div>
+        <br />
+        <asp:Button runat ="server" Text="Translate" OnClick="btn8Clicked"/>
+        <br />
+        <br />
+        <div class="form-group">
+            <asp:Label runat ="server" Text="Output"></asp:Label>
+            <br />
+            <asp:TextBox ID="output" CssClass="form-control" runat="server" Width="800" Height="100"></asp:TextBox>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ taskC8/default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace taskC8
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            input.Font.Name = "Inconsolata";
+            input.Font.Size = 16;
+        }
+        protected void btn8Clicked(object sender, EventArgs e)
+        {
+            Dictionary<char, String> morseCode = new Dictionary<char, String>()
+            {
+                {'a' , ".-"},{'b' , "-..."},{'c' , "-.-."},
+                {'d' , "-.."},{'e' , "."},{'f' , "..-."},
+                {'g' , "--."},{'h' , "...."},{'i' , ".."},
+                {'j' , ".---"},{'k' , "-.-"},{'l' , ".-.."},
+                {'m' , "--"},{'n' , "-."},{'o' , "---"},
+                {'p' , ".--."},{'q' , "--.-"},{'r' , ".-."},
+                {'s' , ".-."},{'t' , "-"},{'u' , "..-"},
+                {'v' , "...-"},{'w' , ".--"},{'x' , "-..-"},
+                {'y' , "-.--"},{'z' , "--.."},
+                {' ', "/" },
+                {'0' , "-----"},{'1' , ".----"},{'2' , "..----"},{'3' , "...--"},
+                {'4' , "....-"},{'5' , "....."},{'6' , "-...."},{'7' , "--..."},
+                {'8' , "---.."},{'9' , "----."},
+            };
+            string inputText = input.Text;
+            inputText = inputText.ToLower();
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                char singleChar = inputText[i];
+                if (morseCode.ContainsKey(singleChar))
+                {
+                    output.Text += (morseCode[singleChar]) + " ";
+                    output.Font.Name = "Inconsolata";
+                    output.Font.Size = 20;
+                }
+            }
+        }
+    }
+}
